6a475cad3df07ce33372d5776cd1f780ee2482b1
suihongwei <18845795735@163.com>
2021-08-03 12:24:57 +0800

    feat:Add code comments

--- app/src/main/java/com/shawn/imageweightloss/util/ImageWeightLossByLibJpeg.kt
+++ app/src/main/java/com/shawn/imageweightloss/util/ImageWeightLossByLibJpeg.kt
@@ -3,9 +3,17 @@ package com.shawn.imageweightloss.util
 import android.graphics.Bitmap
 
 object ImageWeightLossByLibJpeg {
+
+    /**
+     * 压缩图片
+     * @param bitmap 图片
+     * @param quality 0-100
+     * @param outFilPath 输出地址
+     * @param optimize TRUE=arithmetic coding, FALSE=Huffman
+     */
     external fun compressBitmap(
         bitmap: Bitmap, quality: Int,
-        outFilPath: String?,
+        outFilPath: String,
         optimize: Boolean
     ): Int
 
